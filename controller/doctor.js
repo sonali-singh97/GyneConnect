c69bbdb5a14a64d03c320dad96f477e6be40a332
Sonali Singh <sonali122000@gmail.com>
2021-07-05 11:30:24 +0530

    add search feature

--- controller/doctor.js
+++ controller/doctor.js
@@ -2,15 +2,17 @@ const asyncHandler = require("express-async-handler")
 const Doctor = require("../models/doctor")
 const bcrypt = require("bcryptjs")
 const generateToken = require("../utilities/generateToken");
-const {LocalStorage} = require("node-localstorage");
+const {
+    LocalStorage
+} = require("node-localstorage");
 const doctors_data = require("../doctors_data");
 
-localStorage = new LocalStorage('./scratch'); 
+localStorage = new LocalStorage('./scratch');
 
 const getAllDoctors = asyncHandler(async (req, res) => {
-const doctors = await Doctor.find({})
-return doctors
-//res.status(200).render("find_a_doctor", {doctors})
+    const doctors = await Doctor.find({})
+    return doctors
+    //res.status(200).render("find_a_doctor", {doctors})
 })
 
 // @desc Register Doctor
@@ -40,11 +42,11 @@ const registerDoctor = asyncHandler(async (req, res) => {
         })
     } else {
         const hashedPassword = await bcrypt.hash(password, 12)
-   
+
         const newDoctor = new Doctor({
             fullName,
             email,
-            password : hashedPassword,
+            password: hashedPassword,
             gender,
             hospitalName,
             location,
@@ -65,26 +67,25 @@ const registerDoctor = asyncHandler(async (req, res) => {
 // @desc Login Doctor
 // @route POST /auth/doctor/login
 // @Access Public
-  const loginDoctor = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-  
-    const doctor = await Doctor.findOne({ email });
-    
+const loginDoctor = asyncHandler(async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    const doctor = await Doctor.findOne({
+        email
+    });
+
     if (doctor && (await bcrypt.compare(password, doctor.password))) {
         const jwt_token = "Bearer " + generateToken(doctor._id)
         localStorage.setItem("jwt", jwt_token);
-    //     res.status(200).json({
-    //     fullname: doctor.fullname,
-    //     email: doctor.email,
-    //     isVerified: doctor.isVerified,
-    //     token: jwt_token,
-    //   });
-    res.status(200).redirect("/doctor-login")
+        res.status(200).redirect("/doctor-login")
     } else {
-      res.status(401);
-      throw new Error("Invalid email or password !!");
+        res.status(401);
+        throw new Error("Invalid email or password !!");
     }
-  });
+});
 
 // @desc Logout Doctor
 // @route GET /auth/doctor/login
@@ -92,20 +93,46 @@ const registerDoctor = asyncHandler(async (req, res) => {
 const logoutDoctor = asyncHandler(async (req, res) => {
     localStorage.removeItem("jwt");
     res.status(200).redirect("/")
-  });
+});
 
 
 const getProfile = asyncHandler(async (req, res) => {
-   const doctor = await Doctor.findById(req.params.id)
-   return doctor;
-  });
+    const doctor = await Doctor.findById(req.params.id)
+    return doctor;
+});
 
-module.exports={
+const findNearbyDoctor = asyncHandler(async (req, res) => {
+    console.log(req.body.input)
+    const doctors = await Doctor.find({})
+    const tokens = req.body.input
+        .toLowerCase()
+        .split(' ')
+        .filter(function (token) {
+            return token.trim() !== ''
+        })
+
+    const searchTermRegex = new RegExp(tokens.join('|'), 'gim')
+
+    const doctorsArray = doctors.map(
+        (ele) => `${ele.fullName } ${ele.location} ${ele.tags.join(' ')}  ${ele.address} ${ele.hospitalName} `,
+    )
+    const filteredList = doctors.filter(function (ele, index) {
+        if(doctorsArray[index].match(searchTermRegex))
+           return ele
+        
+    })
+    console.log(filteredList)
+    res.render('find_a_doctor', {
+        doctors : filteredList
+    })
+
+});
+
+module.exports = {
     registerDoctor,
     loginDoctor,
     logoutDoctor,
     getAllDoctors,
-    getProfile
+    getProfile,
+    findNearbyDoctor
 }
-
-
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,80 @@
 const express = require('express')
 const router = express.Router()
-const { ensureAuth, ensureGuest } = require('../middlewares/authMiddleware')
-const {getAllDoctors, getProfile} = require('../controller/doctor')
+const {
+  ensureAuth,
+  ensureGuest
+} = require('../middlewares/authMiddleware')
+const {
+  getAllDoctors,
+  getProfile,
+  findNearbyDoctor
+} = require('../controller/doctor')
 
-// @desc    Login/Landing page
-// @route   GET /
-  router.get('/', ensureGuest, (req, res) => {
-    res.render('index')
+
+router.get('/', ensureGuest, (req, res) => {
+  res.render('index')
+})
+
+router.get('/find_a_doctor', ensureGuest, async (req, res) => {
+  const doctors = await getAllDoctors()
+  res.render('find_a_doctor', {
+    doctors
   })
+})
 
-  router.get('/find_a_doctor', ensureGuest, async(req, res) => {
-    const doctors = await getAllDoctors()
-    res.render('find_a_doctor', {doctors})
+router.route("/search-doctor").all(ensureGuest).post(findNearbyDoctor)
+
+router.get('/review', ensureGuest, (req, res) => {
+  res.render('review')
+})
+
+router.get('/signup', ensureGuest, (req, res) => {
+  res.render('sign_up')
+})
+
+router.get('/resources', ensureGuest, (req, res) => {
+  res.render('resources')
+})
+
+router.get('/report', ensureGuest, (req, res) => {
+  res.render('report')
+})
+
+router.get('/login', ensureGuest, (req, res) => {
+  res.render('log_in')
+})
+
+router.get('/doctor_signup', ensureGuest, (req, res) => {
+  res.render('doctor_sign_up')
+})
+
+router.get('/doctor_login', ensureGuest, (req, res) => {
+  res.render('doctor_log_in')
+})
+
+router.get('/doctor/:id', async (req, res) => {
+  const doctor = await getProfile(req, res)
+  res.render("find_a_doctor_full_profile", {
+    doctor
   })
+})
 
-  router.get('/review', ensureGuest, (req, res) => {
-    res.render('review')
-  })
+router.get('/book', (req, res) => {
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  const endDate = new Date();
+  endDate.setDate(endDate.getDate() + 7);
+  const startDate = new Date()
+  const dates =[]
+  for (var d = startDate; d <= endDate;  d.setDate(d.getDate() + 1)) {
+    const format = `${d.getDate()} ${monthNames[ d.getMonth()] } ${d.getFullYear()}`
+    dates.push(format);
+  }
 
-  router.get('/signup', ensureGuest, (req, res) => {
-    res.render('sign_up')
-  })
-
-  router.get('/resources', ensureGuest, (req, res) => {
-    res.render('resources')
-  })
-
-  router.get('/report', ensureGuest, (req, res) => {
-    res.render('report')
-  })
-
-  router.get('/login', ensureGuest, (req, res) => {
-    res.render('log_in')
-  })
-
-  router.get('/doctor_signup', ensureGuest, (req, res) => {
-    res.render('doctor_sign_up')
-  })
-
-  router.get('/doctor_login', ensureGuest, (req, res) => {
-    res.render('doctor_log_in')
-  })
-
-  router.get('/doctor/:id',async (req, res)=>{
-    const doctor = await getProfile(req, res)
-    res.render("find_a_doctor_full_profile", {doctor})
-  } )
-
-  router.get('/book',  (req, res) => {
-    res.render('book')
-  })
+  res.render('book',{ dates} )
+})
 
 
 
-  module.exports = router
+module.exports = router
